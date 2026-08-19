7cd2546f4c01ea621c67d69957ae2b0c39557e27
PRATIK RAJ <pratik20gb@gmail.com>
2026-08-19 15:22:02 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char ch:s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        for(char ch:t.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)-1);
+        }
+        for(int ch:map.values()){
+            if(ch!=0){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+}
